39cdefe1b50e5eb2412db56e217be0184b104d1d
craxkumar <90772223+craxkumar@users.noreply.github.com>
2023-04-29 16:26:38 +0530

    Fix/dry run issues (#12)
    * Resolved issues from the dry run
    
    * updated the node group configuration
    
    * updaeted the readme file for the eks web UI
    
    * updated script files

--- generators/app/templates/auto-deploy.sh
+++ generators/app/templates/auto-deploy.sh
@@ -12,7 +12,7 @@ function applicationDeployment {
   echo " NOTE:- You will find the DNS name of application load balancer under ./helm dir in output.txt file"
   echo ""
 
-  echo -n "If above requirements meet, Enter(yes):"
+  echo -n "Confirm if you meet all the above requirements,(yes/no):"
   read user_continue_action_appDeployment
   if [ "$user_continue_action_appDeployment" == "yes" ]; then
       cd .. 
@@ -27,12 +27,12 @@ function applicationDeployment {
 
 #AWS
 function aws {
-    echo "You selected AWS"
+    echo -e "You have opted for \033[1;32mAWS\033[0m cloud provider"
     # Add code here to execute the action for aws
     echo "Initiating the aws action...."
     while true; do
       echo ""
-      echo "Setting up aws configuration which will be used be terraform"
+      echo "Set up AWS credentials which will be used be terraform"
       echo -n "Provide aws access key:"
       read aws_access_key
       echo -n "Provide aws secret key:"
@@ -40,10 +40,10 @@ function aws {
       export AWS_ACCESS_KEY_ID=$aws_access_key
       export AWS_SECRET_ACCESS_KEY=$aws_secret_key
       echo ""
-      echo -e "\033[1;33mProvided aws configuration\033[0m"
+      echo -e "\033[1;33mProvided aws credentials\033[0m"
       echo "aws access key:"${AWS_ACCESS_KEY_ID}
       echo "aws secret key:"${AWS_SECRET_ACCESS_KEY}
-      echo -n "Ensure that your aws configuration are correct, if you agree Enter(yes):"
+      echo -n "Ensure that your aws credentials are correct,(yes/no):"
       read user_confirmation
 
       # convert user input to lowercase
@@ -55,10 +55,10 @@ function aws {
     echo ""
     echo -e "\033[1;32mAws credentials are set!\033[0m"
     echo ""
-    echo -n "Continue with the WDI deployment, if you agree Enter(yes):"
+    echo -n "Continue with the Infrastructure deployment,(yes/no):"
     read user_continue_action
     if [ "$user_continue_action" == "yes" ]; then
-    echo "Deploying the WDI in our AWS cloud....."
+    echo "Deploying the Infrastructure in our AWS cloud....."
         # List of directories to process
         directories=(
         "./eks"
@@ -96,7 +96,7 @@ function aws {
         echo ""
     fi
 
-    echo -n "Do you want to contiune with the application Deployment, if you agree Enter(yes):"
+    echo -n "Do you want to contiune with the application Deployment,(yes/no):"
     read  appDepolyment; 
 
     if [ "$appDepolyment" == "yes" ]; then
@@ -107,87 +107,28 @@ function aws {
 
 }
 
-function azure {
-    echo "You selected AZURE"
-    # Add code here to execute the action for azure
-    echo -e "we are not supporting azure as of now, please come back later!\U0001F600"
-}
-
-function gcp {
-    echo "You selected GCP"
-    # Add code here to execute the action for gcp
-    echo -e "we are not supporting gcp as of now, please come back later!\U0001F600"
-}
-
 ############################ SCRIPT STARTS ##########################
 
 # Check if figlet is installed
-if ! which figlet > /dev/null; then
-    # Install figlet
-    sudo apt-get -y install figlet
-fi
-
-
-# Define the options
-options=("1. AWS" "2. AZURE" "3. GCP")
-
-# Initialize variables
-selected=0
-num_options=${#options[@]}
-
-# Clear the screen
-# clear
-
-# Loop until the user selects an option
-while true; do
-  # Display the options 
-  figlet -f slant "AutoDeploy" 
-  echo "Select the cloud provider"
-  for (( i=0; i<$num_options; i++ )); do
-    if [ $i -eq $selected ]; then
-      echo -e "\033[1;32m> ${options[$i]}\033[0m"
-    else
-      echo "  ${options[$i]}"
+if [ "$(uname -m)" = "x86_64" ]; then
+    if ! which figlet > /dev/null; then
+        # Install figlet using apt-get
+        sudo apt-get -y install figlet
     fi
-  done
-
-  # Get input from the user
-  read -s -n 1 key
-  case "$key" in
-    "A") # Up arrow
-      ((selected--))
-      if [ $selected -lt 0 ]; then
-        selected=$((num_options-1))
-      fi
-      ;;
-    "B") # Down arrow
-      ((selected++))
-      if [ $selected -ge $num_options ]; then
-        selected=0
-      fi
-      ;;
-    "") # Enter key
-      break
-      ;;
-  esac
-
-  # Clear the screen
-  clear
-done
-
-# Display the selected option
-# echo "You selected: ${options[$selected]}"
-
-# Execute different actions based on the selected option
-if [ $selected -eq 0 ]; then
-  aws
-  # Add code here to execute the action for aws
-elif [ $selected -eq 1 ]; then
-  azure
-  # Add code here to execute the action for azure
-elif [ $selected -eq 2 ]; then
-  gcp
-  # Add code here to execute the action for 
+elif [ "$(uname -m)" = "arm64" ]; then
+    if ! which figlet > /dev/null; then
+        # Install figlet using Homebrew
+        brew install figlet
+    fi
 else
-  echo "Invalid selection"
+    echo "Unsupported architecture: $(uname -m)"
+    exit 1
 fi
+
+# Use figlet to print "AutoDeploy"
+figlet -f slant "AutoDeploy"
+
+
+figlet -f slant "AutoDeploy" 
+echo ""
+aws
--- generators/app/templates/ecr-build&publish/02-build-image.tf
+++ generators/app/templates/ecr-build&publish/02-build-image.tf
@@ -18,19 +18,8 @@ resource "null_resource" "build_image" {
             cd "$dir"
             
             echo "CURRENT DIR:-" $(pwd)
-
-            # Execute the command, based on machine type
-            MACHINE_TYPE=`uname -m`
-
-            # Execute the command
-            if [ $MACHINE_TYPE == 'x86_64' ]; then
-                npm run java:docker
-            elif [ $MACHINE_TYPE == 'arm64' ]; then
-                npm run java:docker:arm64
-            else
-                echo "Unsupported machine architecture: $MACHINE_TYPE"
-            fi
-
+            npm run java:docker
+            
             # Change back to the original directory
             cd ..
         done
